055f4dfec6644c057586a5cc59616b07889145f9
Bram-ter <bram.tersteeg2@hva.nl>
2023-02-21 11:06:58 +0100

    break apart a function

--- spa/js/script.js
+++ spa/js/script.js
@@ -36,8 +36,8 @@ async function getData() {
   return data
 }
 
-/** Change the inner html of the h1 to a random value out of the array  **/
-function TitleToJoke() {
+/** Randomizer array gotten from the api  **/
+function randomizeArray() {
   // console.log(data)
   // Random function https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   const randomizer = Math.floor(Math.random()*data.length)
@@ -45,14 +45,19 @@ function TitleToJoke() {
   // Randomize array gotten from data
   const name = data[randomizer].joke
 
+  return name
+}
+
+/** Change the inner html of the h1 to a random value out of the array  **/
+function TitleToJoke(name) {
   // Change HTML element to the randomized array
-  dadJoke.innerHTML = name
+  dadJoke.innerHTML = randomizeArray(name)
 }
 
 /** Change the quote by clicking on the button **/
-function changeQuote(data) {
+function changeQuote() {
   button.addEventListener ('click', () => {
-    TitleToJoke(data)
+    TitleToJoke()
   })
 }
 
