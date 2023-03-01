8ec814a79dc8ca0077ba34daf55207a72d973e0a
Bram-ter <bram.tersteeg2@hva.nl>
2023-03-01 15:27:09 +0100

    add module for data related code

--- spa/js/script.js
+++ spa/js/script.js
@@ -1,20 +1,10 @@
-import getData from "./api.js"
+import logData from "./data.js"
 // import TitleToJoke from "./render.js"
 
 const button = document.querySelector('button')
 const dadJoke = document.querySelector('h1')
 
 changeQuote()
-
-/** Get two arrays with data and combine those **/
-async function logData() {
-  // Get arrays with 10 quotes two times from the api
-  const quote1 = await getData()
-  const quote2 = await getData()
-
-  // Break apart both arrays and return them as a single one
-  return [...quote1, ...quote2]
-}
 
 /** Randomizer array gotten from the api  **/
 async function randomizeArray() {
