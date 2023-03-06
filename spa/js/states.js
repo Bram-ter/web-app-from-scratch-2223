eef742c623a24fa73e53973b95b538ac0bb06330
Bram-ter <bram.tersteeg2@hva.nl>
2023-03-06 21:09:25 +0100

    switch some states

--- spa/js/states.js
+++ spa/js/states.js
@@ -1,16 +1,16 @@
 /** Different states **/
 export function startLoading() {
-  const button = document.querySelector('button')
-
-  button.setAttribute("disabled", true)
-  button.innerHTML = 'Loading bad joke' 
-}
-
-export function disableElement() {
   const heading = document.querySelector('h1')
 
   heading.innerHTML = "A bad joke is on it's way!"
   heading.classList.toggle("disabled")
+}
+
+export function disableElement() {
+  const button = document.querySelector('button')
+
+  button.setAttribute("disabled", true)
+  button.innerHTML = 'Loading bad joke' 
 }
 
 export function stopLoading() {
