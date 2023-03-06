7047a2a8b2556e7089af53191fbe34cece807c97
Bram-ter <bram.tersteeg2@hva.nl>
2023-03-06 10:38:23 +0100

    style states

--- spa/js/states.js
+++ spa/js/states.js
@@ -8,9 +8,14 @@ export function loadingState() {
 
 export function stopLoading() {
   const button = document.querySelector('button')
+  const heading = document.querySelector('h1')
+
+  heading.innerHTML = "A bad joke is on it's way!"
+  heading.classList.toggle("disabled")
 
   setTimeout(() => {
     button.removeAttribute("disabled")
     button.innerHTML = 'Get a bad joke!' 
+    heading.classList.toggle("disabled")
   }, 1000)
 }
